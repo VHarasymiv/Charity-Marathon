b427a47fae85834fd9845a0f38fadfc18cd7714b
VHarasymiv <41122474+VHarasymiv@users.noreply.github.com>
2022-09-23 16:54:42 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -20,44 +20,44 @@ function formatPhoneNumber(value) {
 }
 
 
-
-
 // Success Page
 
-var modal = null;
-var modalSuccess = null;
-const showSuccess = () => {
-
-  if(modalSuccess !== null) {
-    modalSuccess.remove();
-  }
-
-  modalSuccess = document.createElement('div');
-  modalSuccess.innerHTML = `
-  <div class="modal fade " tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-lg">
-    <div class="modal-content success-page">
-    <div>
-      <button type="button" class="modal-icon float-end" data-bs-dismiss="modal" aria-label="Close"></button>
-    </div>
-    <div class="modal-body">
-      <h3 class="">Заявка на реєстрацію успішно відправлена!</h3>
-      <p class="success-text">
-        Дякуємо за те, що долучилися до нашої ініціативи. Менеджер зв’яжеться з вами на протязі дня.
-      </p>
-      <div class="text-center">
-        <img class="story-image" src="images/success-image.png"/>
-      </div>
-    </div>
-  </div>
-</div>
-</div>
-  `;
-
-  document.body.append(modalSuccess);
-  var success = new bootstrap.Modal(modalSuccess.querySelector('.modal'));
-  success.show();
-  // modal.hide();
-}
-
-document.getElementById('form1').addEventListener('submit', showSuccess);
+// var modal = null;
+// var modalSuccess = null;
+// const showSuccess = () => {
+//
+//   if(modalSuccess !== null) {
+//     modalSuccess.remove();
+//   }
+//
+//
+//
+//   modalSuccess = document.createElement('div');
+//   modalSuccess.innerHTML = `
+//   <div class="modal fade " tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+//     <div class="modal-dialog modal-lg">
+//     <div class="modal-content success-page">
+//     <div>
+//       <button type="button" class="modal-icon float-end" data-bs-dismiss="modal" aria-label="Close"></button>
+//     </div>
+//     <div class="modal-body">
+//       <h3 class="">Заявка на реєстрацію успішно відправлена!</h3>
+//       <p class="success-text">
+//         Дякуємо за те, що долучилися до нашої ініціативи. Менеджер зв’яжеться з вами на протязі дня.
+//       </p>
+//       <div class="text-center">
+//         <img class="story-image" src="images/success-image.png"/>
+//       </div>
+//     </div>
+//   </div>
+// </div>
+// </div>
+//   `;
+//
+//   document.body.append(modalSuccess);
+//   var success = new bootstrap.Modal(modalSuccess.querySelector('.modal'));
+//   success.show();
+//   modal.hide();
+// }
+//
+// document.getElementById('form1').addEventListener('submit', showSuccess);
